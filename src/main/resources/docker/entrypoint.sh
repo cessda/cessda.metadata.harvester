f0ae8e1340eaf6641348a2e19b97cab55ae3c700
Matthew Morris <matthew.morris@cessda.eu>
2019-12-13 10:31:00 +0100

    Add configurations for CESSDA. These are fused configurations for common metadata formats.

--- src/main/resources/docker/entrypoint.sh
+++ src/main/resources/docker/entrypoint.sh
@@ -1,2 +1,2 @@
-exec java $JAVA_OPTS -jar ./application.jar --spring.config.additional-location=file:./application.properties
+exec java "$JAVA_OPTS" -jar ./application.jar --spring.config.additional-location=file:./application.properties
 
