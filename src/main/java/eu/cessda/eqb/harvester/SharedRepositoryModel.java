02fa93ff4f2ddcd6744c465efd82795bd2177ca3
Matthew Morris <matthew.morris@cessda.eu>
2022-03-31 10:15:42 +0200

    #409: Add documentation to the SharedRepositoryModel record, check all the SharedRepositoryModel fields are written correctly.

--- src/main/java/eu/cessda/eqb/harvester/SharedRepositoryModel.java
+++ src/main/java/eu/cessda/eqb/harvester/SharedRepositoryModel.java
@@ -2,6 +2,18 @@ package eu.cessda.eqb.harvester;
 
 import java.net.URI;
 
+/**
+ * Repository model to be serialised to JSON. This is used by downstream components.
+ * @param code the identifier of the repository.
+ * @param name the friendly name of the remote repository.
+ * @param url the base URL of the repository.
+ * @param setSpec the set harvested from the repository.
+ * @param metadataPrefix the metadata prefix harvested from the repository.
+ * @param ddiVersion the DDI version harvested from the remote repository.
+ * @param profile the URL of the CMV profile to validate against.
+ * @param validationGate the CMV validation gate to use.
+ * @param defaultLanguage the default language to use when a metadata record doesn't specify a language.
+ */
 public record SharedRepositoryModel(
     String code,
     String name,
--- src/test/java/eu/cessda/eqb/harvester/EQBHarvestingServiceTest.java
+++ src/test/java/eu/cessda/eqb/harvester/EQBHarvestingServiceTest.java
@@ -1,52 +1,66 @@
 package eu.cessda.eqb.harvester;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Collections;
 
 import static java.io.InputStream.nullInputStream;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class EQBHarvestingServiceTest
 {
-	private final Harvester harvester;
+    private static final Repo testRepository = new Repo(
+        Collections.singleton( new Repo.MetadataFormat( null, "oai_ddi", "DDI_2_5", URI.create( "https://cmv.cessda.eu/profiles/cdc/ddi-2.5/latest/profile.xml" ) ) ),
+        "TEST",
+        "Test Repository",
+        URI.create( "http://localhost:8080/v0/oai?set=study_group:paihde" ),
+        false,
+        "en",
+        "BASIC"
+    );
 
-	public EQBHarvestingServiceTest() throws IOException
+	public Harvester getHarvester(Path harvesterDirectory) throws IOException
     {
 		var harvesterConfiguration = new HarvesterConfiguration();
-		harvesterConfiguration.setDir( Path.of("data2") );
+		harvesterConfiguration.setDir( harvesterDirectory );
 		harvesterConfiguration.setFrom( new HarvesterConfiguration.From(null) );
 		harvesterConfiguration.setTimeout( Duration.ofSeconds( 10 ) );
-		var repo = new Repo(
-            Collections.singleton(new Repo.MetadataFormat(null,"oai_ddi", "DDI_2_5", URI.create( "https://cmv.cessda.eu/profiles/cdc/ddi-2.5/latest/profile.xml" ) ) ),
-            "TEST",
-            null,
-            URI.create( "http://localhost:8080/v0/oai?set=study_group:paihde" ),
-            false,
-            "en",
-            "BASIC"
-        );
-		harvesterConfiguration.getRepos().add( repo );
+        harvesterConfiguration.getRepos().add( testRepository );
 
         var httpClient = mock( HttpClient.class );
         when( httpClient.getHttpResponse( any(URI.class) ) ).thenReturn( nullInputStream() );
 
-        harvester = new Harvester( httpClient, harvesterConfiguration, new IOUtilities(), new RepositoryClient( httpClient ) );
+        return new Harvester( httpClient, harvesterConfiguration, new IOUtilities(), new RepositoryClient( httpClient ) );
 	}
 
 	@Test
-	void indexAllTest()
-	{
-		harvester.run(  );
-		assertTrue( Files.exists( Paths.get( "data2" ) ) );
+	void indexAllTest(@TempDir Path tempDir) throws IOException
+    {
+		getHarvester( tempDir ).run();
+
+        // Read pipeline.json, assert fields are as expected.
+        var pipeline = new ObjectMapper().readValue( new File(tempDir + "/wrapped/TEST/oai_ddi/pipeline.json"), SharedRepositoryModel.class );
+        assertEquals( testRepository.code(), pipeline.code() );
+        assertEquals( testRepository.name(), pipeline.name() );
+        assertEquals( testRepository.url(), pipeline.url() );
+        assertEquals( testRepository.validationGate(), pipeline.validationGate() );
+        assertEquals( testRepository.defaultLanguage(), pipeline.defaultLanguage() );
+
+        // Validate the metadata fields are as expected.
+        var metadata = testRepository.metadataPrefixes().stream().findAny().orElseThrow();
+        assertEquals( metadata.metadataPrefix(), pipeline.metadataPrefix() );
+        assertEquals( metadata.setSpec(), pipeline.setSpec() );
+        assertEquals( metadata.ddiVersion(), pipeline.ddiVersion() );
+        assertEquals( metadata.validationProfile(), pipeline.profile() );
 	}
 }
