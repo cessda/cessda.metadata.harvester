014a8a3e57559e16b6887043a1d99f6edf3ff142
Toni Sissala <toni.sissala@tuni.fi>
2023-04-28 12:57:48 +0000

    Revert "Attempt to fix some checkstyle errors in four .java files"
    This reverts commit 219896097893da9b7b136e96248fcaefb4af36fc.

--- src/main/java/eu/cessda/oaiharvester/DirectoryCreationFailedException.java
+++ src/main/java/eu/cessda/oaiharvester/DirectoryCreationFailedException.java
@@ -9,9 +9,9 @@ package eu.cessda.oaiharvester;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,27 +27,27 @@ import java.nio.file.Path;
 /**
  * Thrown when creating the output directory fails.
  */
-class DirectoryCreationFailedException extends Exception {
-
+class DirectoryCreationFailedException extends Exception
+{
     @Serial
     private static final long serialVersionUID = 1L;
 
     private final String directory;
 
     /**
-     * Constructs a {@link DirectoryCreationFailedException} with the specified
-     * directory and cause.
+     * Constructs a {@link DirectoryCreationFailedException} with the specified directory and cause.
      *
      * @param directory the directory that could not be created.
      * @param cause     the exception that caused this exception.
      */
-    DirectoryCreationFailedException(final Path directory,
-                                     final Throwable cause) {
-        super("Creating " + directory + " failed", cause);
+    DirectoryCreationFailedException( Path directory, Throwable cause )
+    {
+        super( "Creating " + directory + " failed", cause );
         this.directory = directory.toString();
     }
 
-    Path getDirectory() {
+    Path getDirectory()
+    {
         return Path.of(directory);
     }
 }
