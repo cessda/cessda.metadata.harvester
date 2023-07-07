791c316fc3a91c9c96ab279bccd56676904a1664
Matthew Morris <matthew.morris@cessda.eu>
2023-07-07 11:45:46 +0200

    #569: Document "Repo" in the README and JavaDocs

--- src/main/java/eu/cessda/oaiharvester/Repo.java
+++ src/main/java/eu/cessda/oaiharvester/Repo.java
@@ -9,9 +9,9 @@ package eu.cessda.oaiharvester;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,13 +27,16 @@ import java.util.Set;
 
 /**
  * A configuration of a remote repository.
+ *
  * @param metadataPrefixes The metadata prefixes to harvest from the repository.
- * @param code The identifier of the repository.
- * @param name The friendly name of the remote repository.
- * @param url The base URL of the repository.
- * @param discoverSets If true, the repository should be queried for the sets contained.
- * @param defaultLanguage The default language to be used in the UI.
- * @param validationGate The validation gate to use.
+ * @param code             The identifier of the repository.
+ * @param name             The friendly name of the remote repository.
+ * @param url              The base URL of the repository.
+ * @param discoverSets     If true, the repository should discover sets in the repository and harvest them individually.
+ *                         If false, the repository should harvest without using sets.
+ *                         If {@link MetadataFormat#setSpec} is not {@code null}, that set will override this setting.
+ * @param defaultLanguage  The language to treat metadata if unspecified.
+ * @param validationGate   The validation gate to use. See <a href="https://cmv.cessda.eu/documentation/constraints.html">the CMV documentation</a> for the definition of valid constraints.
  */
 public record Repo(
     Set<MetadataFormat> metadataPrefixes,
@@ -45,5 +48,19 @@ public record Repo(
     String validationGate
 ) implements Serializable
 {
-    record MetadataFormat(String setSpec, String metadataPrefix, String ddiVersion, URI validationProfile) implements Serializable {}
+    /**
+     * A specific harvesting configuration for a repository.
+     *
+     * @param metadataPrefix    the metadata prefix to harvest. This is a mandatory parameter.
+     * @param setSpec           the set to harvest, or {@code null} if no specific set should be harvested.
+     * @param ddiVersion        the DDI version harvested.
+     * @param validationProfile the CMV profile to validate against.
+     */
+    record MetadataFormat(
+        String metadataPrefix,
+        String setSpec,
+        String ddiVersion,
+        URI validationProfile
+    ) implements Serializable {
+    }
 }
